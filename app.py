c75c293e018dcec078d174bffee710b9307f7d9f
seedlinggary <seedling.gary@gmail.com>
2026-04-19 21:41:40 +0300

    updated requreiments.txt

--- app.py
+++ app.py
@@ -163,9 +163,12 @@
 # #     except Exception as e:
 # #         return jsonify({"error": str(e)}), 500
 
-from flask import Flask, request, jsonify
-from geoutils import check_point
+from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
+
+CORS(app)
 
 # 1. Simple GET endpoint
 @app.route("/", methods=["GET"])
