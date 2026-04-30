cea471c2813ad848f6be1d334a3a73a8077dcc14
seedlinggary <seedling.gary@gmail.com>
2026-04-30 15:18:14 +0300

    webscrape

--- geoutils.py
+++ geoutils.py
@@ -35,7 +35,7 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("SUPABASE_DB_URL")
-# print("DATABASE_URL:", DATABASE_URL)
+print("DATABASE_URL:", DATABASE_URL)
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
