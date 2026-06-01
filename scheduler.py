8f5a06df92710aaef935bea3907856b405f2a3aa
seedlinggary <seedling.gary@gmail.com>
2026-06-01 10:13:27 +0300

    updating webscraping

--- scheduler.py
+++ scheduler.py
@@ -27,7 +27,7 @@ def start_scheduler() -> None:
         return
     _scheduler.add_job(
         func=_scheduled_scrape,
-        # trigger=IntervalTrigger(minutes=6),
+        trigger=IntervalTrigger(minutes=50),
         trigger=IntervalTrigger(hours=240),
         id="zoopla_auto_scrape",
         replace_existing=True,
